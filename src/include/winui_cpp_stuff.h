37fa36600d3c11ce977dda0a43f500a7033c05b0
poyonshot <poyonshot@gmail.com>
2023-04-22 20:10:27 +0900

    初回

---
+++ src/include/winui_cpp_stuff.h
@@ -0,0 +1,14 @@
+// Copyright (c) 2023 poyonshot.
+// Licensed under the MIT License.
+
+#pragma once
+
+/*
+子ウインドウかどうか調べます。(IsChild)
+最小化されているのか調べます。(IsIconic)
+最大化されているのか調べます。(IsZoomed)
+ウインドウの表示状態を調べます。(IsWindowVisible)
+ウインドウの有効状態を調べます。(IsWindowEnabled)
+ウインドウが Unicode タイプか調べます。(IsWindowUnicode)
+ウインドウのハンドルが有効か調べます。(IsWindow)
+*/
